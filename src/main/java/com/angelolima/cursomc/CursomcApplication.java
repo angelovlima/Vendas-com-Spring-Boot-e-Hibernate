e57112d21165ac90755d32725f598cebacf904d0
angelovlima <angelovictor2001@hotmail.com>
2020-04-30 17:37:40 -0300

    Testando REST

--- src/main/java/com/angelolima/cursomc/CursomcApplication.java
+++ src/main/java/com/angelolima/cursomc/CursomcApplication.java
@@ -1,9 +1,15 @@
 package com.angelolima.cursomc;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
 
 @SpringBootApplication
+@EnableAutoConfiguration
+
+@ComponentScan("com.angelolima.cursomc.resources")
 public class CursomcApplication {
 
 	public static void main(String[] args) {
