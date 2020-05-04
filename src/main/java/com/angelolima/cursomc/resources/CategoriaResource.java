714f8e10090697db81042122acd9135293340ddb
angelovlima <angelovictor2001@hotmail.com>
2020-05-04 13:31:39 -0300

    Testando a classe de domínio Categoria

--- src/main/java/com/angelolima/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/angelolima/cursomc/resources/CategoriaResource.java
@@ -4,12 +4,27 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.angelolima.cursomc.domain.Categoria;
+
+
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
 	@GetMapping
-	public String listar() {
-		return "REST está funcionando!";
+	public List<Categoria> listar() {
+		
+		
+		Categoria cat = new Categoria(1, "Informatica");
+		Categoria cat2 = new Categoria(2, "Escritório");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat);
+		lista.add(cat2);
+		
+		return lista;
 	}
 }
